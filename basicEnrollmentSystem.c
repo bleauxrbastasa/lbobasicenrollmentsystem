f71bb60115fce81d38a52485b705757a2b0f2405
bleauxrbastasa <72042249+bleauxrbastasa@users.noreply.github.com>
2023-04-08 19:26:29 +0800

    Create basicEnrollmentSystem.c

---
+++ basicEnrollmentSystem.c
@@ -0,0 +1,38 @@
+Boolean login():
+    //RETURNS A BOOLEAN TRUE OR FALSE VALUE, IF LOGIN IS EQUAL TO ACCOUNT CREDENTIALS
+    
+    int password = 1234, inputPassowrd;
+    char username = "test", inputUsername;
+    Boolean loginIsValid;
+
+    printf("Please input Login")
+    if(password==1234 && username==1234){
+        printf("Login Success")
+        loginIsValid=true
+    }
+    else{
+
+    }
+
+
+    return loginIsValid
+
+int chooseCourse(){
+    //
+    int Fees=0
+    Fees = determineFees()
+    return Fees
+}
+
+void uiCoursePanel(){
+    //SHOW THE PANEL FOR COURSE OFFERINGS
+}
+
+int determineFees(){
+
+}
+
+int main():
+    login()
+    print(chooseCourse)
+    //MAIN FUNCTION
